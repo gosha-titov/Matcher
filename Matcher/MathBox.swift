5f239ee9b62d5d7b8896035004d4f8f3168c40a4
gosha <gosha.titov@icloud.com>
2022-09-24 02:40:22 +0300

    Refactor: change the lis algorithm to the fastest one.

--- Matcher/MathBox.swift
+++ Matcher/MathBox.swift
@@ -15,34 +15,56 @@ final class MathBox {
     ///     let subsequence = findLis(in: sequence) // [0, 1, 3]
     ///
     /// The example sequence has two lises: `[2, 6, 8]` and `[0, 1, 3]`.
-    /// Therefore, this method chooses the smallest one.
+    /// Therefore, this method returns the smallest one.
     ///
     static func findLis(in sequence: Sequence) -> Subsequence {
         
-        func chooseBest(_ first: Sequence, _ second: Sequence) -> Sequence {
-            let length1 = first.count, length2 = second.count
-            let firstIsBetter = (length1 > length2) || (length1 == length2) && (first.last! < second.last!)
-            return firstIsBetter ? first : second
-        }
-        
         guard sequence.count > 1 else { return sequence }
         
-        var lises = [Subsequence]()
-        var resultLis = Subsequence()
+        // Technically, this array contains the found lises of each length for the current step.
+        // Lises will be ordered by the last element. The length of next lis is one longer.
+        // In this case, the longest lis is the last one.
+        //
+        // Example: sequence = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7]
+        // `lises` will be [[0], [0, 1], [0, 1, 3], [0, 1, 3, 7], [0, 2, 6, 9, 11]]
+        //
+        var lises: [Subsequence] = [[sequence.first!]]
         
-        for current in sequence.enumerated() {
-            var bestLis = Subsequence()
-            for previous in sequence[..<current.offset].enumerated().reversed()
-            where current.element > previous.element {
-                let previousLis = lises[previous.offset]
-                bestLis = chooseBest(bestLis, previousLis)
+        for element in sequence[1...] {
+            
+            var lowerBound = 0, upperBound = lises.count - 1
+            var elementIsNotFound = true
+            
+            // The point of this algorithm is that we do know that `lises` are ordered by the last element.
+            // Shift the boundaries to the first element that is bigger than the current one.
+            // Use binary search which is the fastest.
+            while lowerBound < upperBound {
+                let middle = lowerBound + (upperBound - lowerBound) / 2
+                let middleElement = lises[middle].last!
+                
+                // If we find an equal element, then we already know for sure that no changes can be made.
+                guard middleElement != element else { elementIsNotFound = false; break }
+                
+                if middleElement > element { upperBound = middle }
+                else { lowerBound = middle + 1 }
             }
-            bestLis.append(current.element)
-            lises.append(bestLis)
-            resultLis = chooseBest(resultLis, bestLis)
+            guard elementIsNotFound else { continue }
+            
+            let index = lowerBound
+            
+            // If all elements are smaller, then we add a new lis.
+            // If all elements are bigger, then we change the first lis.
+            // In any other case, we change the selected lis.
+            if index == lises.count - 1, element > lises[index].last! {
+                lises.append(lises[index] + [element])
+            } else if index == 0 {
+                lises[0] = [element]
+            } else {
+                lises[index] = lises[index - 1] + [element]
+            }
         }
         
-        return resultLis
+        return lises.last!
     }
     
     
