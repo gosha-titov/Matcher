7abe03f8e649c33beaf5561df8a1909efdcf46a8
gosha <gosha.titov@icloud.com>
2022-09-23 21:21:26 +0300

    Create Matcher framework.

---
+++ Matcher/Matcher.h
@@ -0,0 +1,18 @@
+//
+//  Matcher.h
+//  Matcher
+//
+//  Created by Gosha on 23.09.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Matcher.
+FOUNDATION_EXPORT double MatcherVersionNumber;
+
+//! Project version string for Matcher.
+FOUNDATION_EXPORT const unsigned char MatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Matcher/PublicHeader.h>
+
+
