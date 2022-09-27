112a4cb6cb18318062310b285cfd5bbef12299ea
gosha <gosha.titov@icloud.com>
2022-09-28 01:47:53 +0300

    Feat: add required and acceptable quantities.

--- Matcher/Models/Configuration.swift
+++ Matcher/Models/Configuration.swift
@@ -1,5 +1,63 @@
+import ModKit
+
 /// A configuration that [typified text](TypifiedText) should conform to.
 struct Configuration {
+    
+    // MARK: Required and Acceptable Quantities
+    
+    /// The quantity indicating the required number of matching chars.
+    ///
+    /// The typified text is considered incorrect if its count of matching chars is less than this quantity.
+    /// If this quantity is `nil` then the check will not be performed.
+    ///
+    var requiredQuantityOfMatchingChars: CharQuantity?
+    
+    /// The quantity indicating the acceptable number of wrong chars.
+    ///
+    /// The typified text is considered incorrect if its count of wrong chars is more than this quantity.
+    /// If this quantity is `nil` then the check will not be performed.
+    ///
+    var acceptableQuantityOfWrongChars: CharQuantity?
+    
+    /// The quantity indicating the number of some chars.
+    enum CharQuantity {
+        
+        /// `all` = 100%, `high` = 75%, `half` = 50%, `low` = 25%, `zero` = 0%
+        case all, high, half, low, zero
+        case other(Double)
+        
+        /// Calculates `Integer` value for this coefficient.
+        func calculateValue(for length: Int) -> Int {
+            let coefficient: Double
+            switch self {
+            case .other(let value): coefficient = value.clamped(to: 0...1.0)
+            case .all:  coefficient = 1.0
+            case .high: coefficient = 0.75
+            case .half: coefficient = 0.5
+            case .low:  coefficient = 0.25
+            case .zero: coefficient = 0.0
+            }
+            return (length.toDouble * coefficient).rounded().toInt
+        }
+    }
+    
+    
+    // MARK: Letter Case Action
+    
+    /// The action to be applied to the letter cases of the typified text.
+    ///
+    /// Kinds of action:
+    ///
+    /// - **doNotChange**: Letter cases will not be changed. There is a mistake if letter cases do not match.
+    ///
+    /// + **leadTo(Kind)**: Letter cases will be leaded to the given kind. There is no mistake if letter cases do not match.
+    ///     - **capitalized**: The writing of a word with its first letter in uppercase and the remaining letters in lowercase.
+    ///     - **uppercase**: The writing of a word in capital letters.
+    ///     - **lowercase**: The writing of a word in small letters.
+    ///
+    /// If this action is `nil` then letter cases will not be changed. There is no mistake if letter cases do not match.
+    ///
+    var letterCaseAction: LetterCaseAction?
     
     /// The kind of action to be applied to the letter cases of the typified text.
     enum LetterCaseAction {
@@ -29,20 +87,5 @@ struct Configuration {
         }
         
     }
-    
-    /// The action to be applied to the letter cases of the typified text.
-    ///
-    /// Kinds of action:
-    ///
-    /// - **doNotChange**: Letter cases will not be changed. There is a mistake if letter cases do not match.
-    ///
-    /// + **leadTo(Kind)**: Letter cases will be leaded to the given kind. There is no mistake if letter cases do not match.
-    ///     - **capitalized**: The writing of a word with its first letter in uppercase and the remaining letters in lowercase.
-    ///     - **uppercase**: The writing of a word in capital letters.
-    ///     - **lowercase**: The writing of a word in small letters.
-    ///
-    /// - **nil**: Letter cases will not be changed. There is no mistake if letter cases do not match.
-    ///
-    var letterCaseAction: LetterCaseAction?
     
 }
